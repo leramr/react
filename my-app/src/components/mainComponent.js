1492fc5e6e090adf817863bd3df86d0e68d8021a
leramr <valeriiamorschavka@gmail.com>
2024-07-29 20:19:16 +0300

    форма

--- my-app/src/components/mainComponent.js
+++ my-app/src/components/mainComponent.js
@@ -1,28 +1,42 @@
 import React, { Component } from 'react'
 
 export default class mainComponent extends Component {
-  constructor (props){
+  constructor(props){
     super(props)
     this.state = {
-      name : ""
+      name:"",
+      email:""
     }
   }
-  changeInput = (e)=> {
-    this.setState ({name: e.target.value})
+  changeClick=(e)=>{
+    console.log(`Name: ${e.target.name}, Value: ${e.target.value}`);
+    this.setState({
+      [e.target.name]:e.target.value
+    })
   }
-  submit =(e)=>{
+
+  clickBtn=(e)=>{
     e.preventDefault()
-    console.log (this.state.name)
+    console.log(`Button clicked with name: ${e.target.name}`);
   }
   render() {
     return (
       <div>
         <form>
+          <input 
+          type='text'
+          name="name"
+          value={this.state.name}
+          onChange={this.changeClick}></input>
+          <input
+          type='text'
+          name="email"
+          value={this.state.email}
+          onChange={this.changeClick}></input>
           <p>{this.state.name}</p>
-          <input onChange={this.changeInput}/>
-          <button onClick={this.submit} type='submit'>sumbit</button>
+          <p>{this.state.email}</p>
+          <button type='submit' onClick={this.clickBtn}>submit</button>
         </form>
-        
       </div>
     )
   }
